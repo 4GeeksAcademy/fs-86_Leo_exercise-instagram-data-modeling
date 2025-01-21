1a06fe050d08e680f78a6146e9d5a5c3053385ad
M.V <marco.mov98@gmail.com>
2025-01-21 12:46:09 +0000

    First Commit

--- src/models.py
+++ src/models.py
@@ -1,11 +1,16 @@
 import os
 import sys
+import enum
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+from sqlalchemy import Enum
 
 Base = declarative_base()
+
+class MediaType(enum.Enum):
+    IMAGE = "image"
 
 class Person(Base):
     __tablename__ = 'person'
@@ -27,7 +32,42 @@ class Address(Base):
 
     def to_dict(self):
         return {}
+    
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250))
+    lastname = Column(String(250))
+    email = Column(String(250), nullable=False)
+    posts = relationship("Post", cascade="all, delete-orphan")
 
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    title = Column(String(250))
+    comments = relationship("Comment", cascade="all, delete-orphan")
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum(MediaType), nullable=False)
+    url = Column(String(250))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id =Column(Integer, ForeignKey('user.id'))
 ## Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
